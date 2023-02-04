5f9d1b3f030c82a40743c30f7810e4d15466afd5
OlegPlugaru <oleg.plugaru.md@gmail.com>
2023-02-04 14:21:14 +0200

    writed some basic programms

---
+++ answer.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // Prompt user for answer
+    char c = get_char("Answer:");
+
+    // Check answer
+    if (c=='Y' || c == 'y')
+    {
+        printf("yes\n");
+    }
+    else if (c == 'N' || c == 'n')
+    {
+        printf("no\n");
+    }
+}
